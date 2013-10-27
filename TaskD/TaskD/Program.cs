a21773a2e93a9764bd2538c731ba07a4e2adb84c
James Cooper <james@coopernets.co.uk>
2013-10-27 14:07:06 +0000

    Complete Branch

--- TaskD/TaskD/Program.cs
+++ TaskD/TaskD/Program.cs
@@ -8,8 +8,62 @@ namespace TaskD
 {
     class Program
     {
+        static public double length = 0;
+        static public double width = 0;
         static void Main(string[] args)
         {
+
+            ConsoleKeyInfo ck;
+            Console.WriteLine(" Rectangle Checker  ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1 | Area Check      ");
+            Console.WriteLine("2 | Perimeter Check ");
+            Console.WriteLine("3 | Exit            ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Please Select: ");
+            ck = Console.ReadKey(false);
+            if (ck.Key == ConsoleKey.D1)
+            {
+                LengthWidthCheck();
+                double area = AreaCheck(length, width);
+                Console.WriteLine("The Area is: " +  area.ToString());
+                Console.ReadLine();
+
+            }
+            else if (ck.Key == ConsoleKey.D2)
+            {
+                LengthWidthCheck();
+                double perim = PerimCheck(length, width);
+                Console.WriteLine("The Perimeter is: " + perim.ToString());
+                Console.ReadLine();
+
+            }
+            else if (ck.Key == ConsoleKey.D3)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        
+        static double AreaCheck(double l, double w)
+        {
+            double result;
+            result = l * w;
+            return result;
+        }
+        static double PerimCheck(double l, double w)
+        {
+            double result;
+            result = (l * 2) + (w * 2);
+            return result;
+        }
+        static void LengthWidthCheck()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter Length: ");
+            length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Width: ");
+            width = double.Parse(Console.ReadLine());
         }
     }
 }
